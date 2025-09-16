8e4e708b26c5d9f814f017b91a3f708675fc3a5c
catttttttt <ameertaimoorahmad@gmail.com>
2025-09-15 21:46:55 -0400

    Add files via upload

---
+++ public/script.js
@@ -0,0 +1,10 @@
+    function checkFunction() {
+      window.location.href = "main.html";
+    }
+    function settingsFunction() {
+      window.location.href = "settings.html";
+    }
+     function homeFunction() {
+      window.location.href = "home.html";
+    }
+
