92d0030ac22a02e950438aa8185c12ab84b89921
catttttttt <ameertaimoorahmad@gmail.com>
2025-09-16 21:08:46 -0400

    Create script.js

---
+++ public/script.js
@@ -0,0 +1,12 @@
+    function checkFunction() {
+      window.location.href = "main.html";
+    }
+    function settingsFunction() {
+      window.location.href = "settings.html";
+    }
+     function homeFunction() {
+      window.location.href = "home.html";
+    }
+     function gamesFunction() {
+      window.location.href = "home.html";
+    }
